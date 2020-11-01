fa6ed10855aa930461a691cf02bb0003f34b2da1
Daniel Brito <daniellimabrito@gmail.com>
2020-10-31 23:44:59 -0700

    Setup angular app - deploy to CI

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/v1/'
+  apiUrl: 'https://routineorganizer.azurewebsites.net/v1/',
+  apiUrl1: 'https://localhost:5001/v1/'
 };
 
 /*
